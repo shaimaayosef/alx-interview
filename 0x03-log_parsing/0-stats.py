cca65d6d7e64be9a2005d44eb9fe72f9007eabc0
shaimaayosef <shimaa.sayed8888@gmail.com>
2024-06-20 13:19:33 +0300

    4

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -2,38 +2,31 @@
 """
 Log parsing
 """
-
 import sys
+import re
 
-if __name__ == '__main__':
+def print_stats(stats, filesize):
+    print("File size: {}".format(filesize))
+    for k in sorted(stats.keys()):
+        print("{}: {}".format(k, stats[k]))
 
-    filesize, count = 0, 0
-    codes = ["200", "301", "400", "401", "403", "404", "405", "500"]
-    stats = {k: 0 for k in codes}
-
-    def print_stats(stats: dict, file_size: int) -> None:
-        print("File size: {:d}".format(filesize))
-        for k, v in sorted(stats.items()):
-            if v:
-                print("{}: {}".format(k, v))
-
-    try:
-        for line in sys.stdin:
+try:
+    count = 0
+    filesize = 0
+    stats = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0, "404": 0, "405": 0, "500": 0}
+    pattern = re.compile(r'\S+ - \[(.*?)\] "GET /projects/260 HTTP/1.1" (\d{3}) (\d+)')
+    
+    for line in sys.stdin:
+        if pattern.match(line):
             count += 1
-            data = line.split()
-            try:
-                status_code = data[-2]
-                if status_code in stats:
-                    stats[status_code] += 1
-            except BaseException:
-                pass
-            try:
-                filesize += int(data[-1])
-            except BaseException:
-                pass
+            data = pattern.findall(line)[0]
+            status_code = data[1]
+            if status_code in stats:
+                stats[status_code] += 1
+            filesize += int(data[2])
             if count % 10 == 0:
                 print_stats(stats, filesize)
-        print_stats(stats, filesize)
-    except KeyboardInterrupt:
-        print_stats(stats, filesize)
-        raise
+    print_stats(stats, filesize)
+except KeyboardInterrupt:
+    print_stats(stats, filesize)
+    raise
