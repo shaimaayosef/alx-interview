c1db06a8f4d0e1892aacd93143b5dd0858be8606
shaimaayosef <shimaa.sayed8888@gmail.com>
2024-06-27 16:16:06 +0000

    1

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,43 +1,32 @@
 #!/usr/bin/python3
-"""
-UTF-8 Validation
-"""
+"""UTF-8 Validation"""
+
+
+def get_leading_set_bits(num):
+    """returns the number of leading set bits (1)"""
+    set_bits = 0
+    helper = 1 << 7
+    while helper & num:
+        set_bits += 1
+        helper = helper >> 1
+    return set_bits
+
 
 def validUTF8(data):
-    # Number of bytes in the current UTF-8 character
-    n_bytes = 0
-
-    # For each integer in the data array
-    for num in data:
-        # Get the binary representation. We only need the least significant 8 bits
-        # for any given number, so we discard the rest.
-        bin_rep = format(num, '#010b')[-8:]
-
-        # If this is the case then we are to start processing a new UTF-8 character.
-        if n_bytes == 0:
-            # Get the number of 1s in the beginning of the string.
-            # This determines how many bytes we get. 
-            # 1000 0000 -> 0 bytes
-            # 1100 0000 -> 2 bytes
-            # 1110 0000 -> 3 bytes
-            # 1111 0000 -> 4 bytes
-            for bit in bin_rep:
-                if bit == '0': break
-                n_bytes += 1
-
-            # 1 byte characters
-            if n_bytes == 0:
+    """determines if a given data set represents a valid UTF-8 encoding"""
+    bits_count = 0
+    for i in range(len(data)):
+        if bits_count == 0:
+            bits_count = get_leading_set_bits(data[i])
+            '''1-byte (format: 0xxxxxxx)'''
+            if bits_count == 0:
                 continue
-        
-        # Else, we are to validate the current UTF-8 character.
-        else:
-            # Else, only the 2 most significant bits should be 10.
-            if not (bin_rep[0] == '1' and bin_rep[1] == '0'):
+            '''a character in UTF-8 can be 1 to 4 bytes long'''
+            if bits_count == 1 or bits_count > 4:
                 return False
-
-        # We reduce the number of bytes to process by 1 after each integer.
-        n_bytes -= 1
-
-    # This is for the case where we might not have the complete data for
-    # a particular UTF-8 character.
-    return n_bytes == 0
+        else:
+            '''checks if current byte has format 10xxxxxx'''
+            if not (data[i] & (1 << 7) and not (data[i] & (1 << 6))):
+                return False
+        bits_count -= 1
+    return bits_count == 0
